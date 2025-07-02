02f08b89d92cd7c170d63a746e950e87eba63ac6
AntoineLandrieux <antoldrx.contact@gmail.com>
2025-07-02 11:45:29 +0200

    Update theme.css and res/git/image.png, Fix GameOver()

---
+++ extensions/Example.js
@@ -0,0 +1,13 @@
+
+window.onload = function (__init__) {
+
+    /**
+     * 
+     * Extensions:
+     * Add JS files into `extensions/` folder to create an extension;
+     * 
+     * FIXME: Not implemented yet.
+     * 
+     */
+
+};
