f5c33318f011ed4b72f1ba4b4314bef725d910b7
slohwnix <slohwnix@gmail.com>
2025-07-09 16:32:20 +0200

    Update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -4,34 +4,32 @@ import platform
 import shutil
 
 directory = os.path.dirname(os.path.abspath(__file__))
-separator = ';' if platform.system() == 'Windows' else ':'
+separator = ";" if platform.system() == "Windows" else ":"
 
 resources = [
-    ('resources', 'resources'),
-    ('theme', 'theme'),
-    ('web', 'web'),
-    ('offline', 'offline'),
-    ('version.json', '.'),
-    ('Update.py', '.'),
-    ('bridge.py', '.'),
-    ('AppEngine.py', '.'),
+    ("resources", "resources"),
+    ("theme", "theme"),
+    ("web", "web"),
+    ("offline", "offline"),
+    ("version.json", "."),
+    ("src", "src"),
 ]
 
 args = [
-    'main.py',
-    '--name=main',
-    '--noconfirm',
-    '--windowed',
-    '--clean',
-    '--distpath=build_win',
-    '--workpath=build/build',
-    '--specpath=build/specs',
+    "main.py",
+    "--name=main",
+    "--noconfirm",
+    "--windowed",
+    "--clean",
+    "--distpath=build_win",
+    "--workpath=build/build",
+    "--specpath=build/specs",
 ]
 
 for src, dest in resources:
     full_src = os.path.abspath(src)
-    args.append(f'--add-data={full_src}{separator}{dest}')
-build_cache_dir = os.path.abspath('build')
+    args.append(f"--add-data={full_src}{separator}{dest}")
+build_cache_dir = os.path.abspath("build")
 if os.path.exists(build_cache_dir):
     print(f"Suppression du dossier cache {build_cache_dir}...")
     shutil.rmtree(build_cache_dir)
